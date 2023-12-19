f6fa1beb4c2cb28e8acbf901c6ca9cf1320dae26
praveen <59041973+praveen9177496079@users.noreply.github.com>
2023-12-19 17:21:05 +0530

    Update setup.py

--- Libraries/python/dabdemo/setup.py
+++ Libraries/python/dabdemo/setup.py
@@ -8,7 +8,7 @@ setup(
   version = dabdemo.__version__,
   author = dabdemo.__author__,
   url = "https://dbc-23992c9f-ce93.cloud.databricks.com",
-  author_email = "praveen20206@gmail.com",
+  author_email = "<my-author-name>@<my-organization>",
   description = "<my-package-description>",
   packages = find_packages(include = ["dabdemo"]),
   entry_points={"group_1": "run=dabdemo.__main__:main"},
